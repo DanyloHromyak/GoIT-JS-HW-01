ddccdb40aada9a9ad309eca263e9359502b6cd98
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-05-21 20:22:56 +0300

    null and unefiend bug fixed

--- hw4.js
+++ hw4.js
@@ -5,7 +5,7 @@ const droidsNumber = +prompt("how many droids u wanna buy?");
 const totalPrice = droidsNumber * pricePerDroid;
 credits = credits - totalPrice;
 
-if (droidsNumber === null) {
+if (droidsNumber == null) {
   console.log("Скасовано користувачем!");
 } else if (credits < 0) {
   console.log("Недостатньо коштів на рахунку!");
